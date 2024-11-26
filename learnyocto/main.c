a9df649143abef24a6beb3d67c7a106880739de4
cxy <45409225+cxy510@users.noreply.github.com>
2024-11-26 17:08:42 +0800

    Add files via upload
    create learnyocto

---
+++ learnyocto/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+    printf ("[master branch]Hello Yocto!\n");//change develop to master
+    printf ("Learn devtool upgrade command\n");
+
+    return 0;
+} /* ----- End of main() ----- */
